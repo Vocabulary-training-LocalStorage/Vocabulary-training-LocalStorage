ec03f40765f81d41adb5cf7cd2dce5b63ff0f310
ramin <zoodfekr.r@gmail.com>
2023-04-12 13:07:13 +0330

    add controler

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useRef, useContext, Suspense } from 'react';
 import Word from './Word';
 import AppContext from '../context/Context';
-
+import { Box, Button, ListItemText } from "@mui/material";
+import { MdRecordVoiceOver } from "react-icons/md";
 const Words = () => {
 	const { datawords, costomcolor, SearchParams } = useContext(AppContext);
 
@@ -15,24 +16,32 @@ const Words = () => {
 			return true;
 		}
 	}
+
+
+
 	return (
-		<div className="words">
-			<ol className=" justify-content-start d-flex flex-wrap p-0 ">
-				{datawords ?
-					datawords
-						.filter(handlesearch)
-						.map(datawords => {
-							return (
-								// <div className='border border-danger d-flex justify-content-end'>
+
+		<>
+
+			<div className="words">
+
+				<ol className=" justify-content-start d-flex flex-wrap p-0 ">
+					{datawords ?
+						datawords
+							.filter(handlesearch)
+							.map(datawords => {
+								return (
+									// <div className='border border-danger d-flex justify-content-end'>
 									<li className=" xs-12 sm-3 li">
 										<Word datawords={datawords} costomcolor={costomcolor} ></Word>
 									</li>
-								// </div>
-							)
-						}).reverse() : null}
-			</ol>
-		</div>
+									// </div>
+								)
+							}).reverse() : null}
+				</ol>
+			</div>
 
+		</>
 	)
 };
 
